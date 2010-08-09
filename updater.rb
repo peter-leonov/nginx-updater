49f9b9612e479114bd81e8baa1c8da59c21fdd7d
Peter Leonov <gojpeg@gmail.com>
2010-08-09 17:34:29 +0400

    check the working directory for cleanness and then checkout appropriate git branch

--- updater.rb
+++ updater.rb
@@ -1,6 +1,36 @@
 #!/usr/bin/env ruby1.9
 
+class NginxUpdater
 
+  module Config
+    NGINX_SRC_PATH = "/Users/peter/pro/nginx/"
+  end
+  
+  def initialize
+    @branches = {"0.8" => "master"}
+  end
+  
+  def run
+    Dir.chdir(Config::NGINX_SRC_PATH)
+    
+    unless `git st` =~ /working directory clean/
+      warn "error: working directory is not clean"
+      return 1
+    end
+    
+    @branches.each do |k, v|
+      update_branch k, v
+    end
+    
+    return 0
+  end
 
-puts "Hello, nginx developer!"
+  def update_branch nginx_branch, git_branch
+    system("git checkout #{git_branch}")
+    
+    
+  end
+  
+end
 
+exit NginxUpdater.new.run
